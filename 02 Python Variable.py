a02ab675cb3d2931b1efd2787339a918a7de55f1
ceobilal <mr.muhammadbilal2020@gmail.com>
2025-10-30 22:36:32 +0500

    Added Python Variable and Data Types files

---
+++ 02 Python Variable.py
@@ -0,0 +1,22 @@
+# Method 1
+Fruit="Mango"
+print(Fruit)
+
+#Method 2
+name="Bilal"
+print(name + "good")
+print(name, "good")
+print("hello",name)
+
+# Method 3
+name="ali"
+name=20
+print(name)
+
+#Method 4
+a="hassan"
+b="ali"
+c=a+b ; d=a,b ; print(a+b)
+
+print(c)
+print(d)
---
+++ 03 Python Data Types.py
@@ -0,0 +1,4 @@
+a="hello world"   # staring 
+b=251             # integer numbers
+c=20.2            # float → decimal numbers
+d=True            # Boolean
