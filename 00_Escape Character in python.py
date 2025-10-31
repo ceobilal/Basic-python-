4db2a36be96ff48dd86729ccf3f1499336492850
ceobilal <mr.muhammadbilal2020@gmail.com>
2025-10-31 22:56:50 +0500

    update

--- 00_Escape Character in python.py
+++ 00_Escape Character in python.py
@@ -1,5 +1,5 @@
 Esacp_Seqence_character="""
-hello\nworld  
+hello\nworld   
 hello\tworld
 hello\"world
 hello\"world\"
@@ -9,3 +9,25 @@ hello\\world
 hello\\world\\
 """
 print(Esacp_Seqence_character)
+
+a="hello\nworld"       # line break 
+b="hello\tworld"       # tab space  (hello   world)
+c="hello\"world"       #  (hello"world)
+d="hello\"world\""     #  hello"world")
+e="hello\'world"       # (hello'world)
+f="hello\'world\'"     # (hello'world')
+g="hello\\world"       # hello\world 
+h="hello\\world\\"     # hello\world\
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+print(a) ; print(b) ; print(c) ; print(d) ; print(e) ; print(f) ; print(g) ; print(h)
+
+print(a,b,c,d,e,f,g,h)
